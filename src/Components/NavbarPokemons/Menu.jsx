661d5469a23572ff85cdfe0ae8ffefe6fcb87279
Jose Corredor <jos3.corredor@gmail.com>
2023-11-10 00:52:44 -0500

    se agrega vista de pokemon seleccionado, y se utiliza la funcion active de navlink

--- src/Components/NavbarPokemons/Menu.jsx
+++ src/Components/NavbarPokemons/Menu.jsx
@@ -3,12 +3,13 @@ import "./Menu.css"
 import { Navbar } from "react-bootstrap"
 
 const Menu = () => {
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : undefined);
   return (
     <Navbar className="navbarpokemons d-flex justify-content-between p-5">
       <img className="icon" src="./icon.svg" alt="icono" />
       <div className="d-flex gap-3">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/pokemons">Pokemones</NavLink></div> 
+      <NavLink style={{ textDecoration: 'none' }} className={ setActiveClass } to="/">Home</NavLink>
+      <NavLink style={{ textDecoration: 'none' }} className={ setActiveClass } to="/pokemons">Pokemones</NavLink></div> 
     </Navbar>
   )
 }
--- src/views/Select/SelectPokemons.jsx
+++ src/views/Select/SelectPokemons.jsx
@@ -1,24 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import { useParams } from "react-router-dom"
 
 const SelectPokemons = () => {
     const { name } = useParams();
-    const [Pokemon, setPokemon] = useState({})
-    const urlApi = `https://pokeapi.co/api/v2/pokemon/${name}`
-
-    const getdata = async () => {
-    const response = await fetch(urlApi)
-    const data = await response.json()
-    console.log(data)
-    setPokemon(data)
+    const [PokemonChoise, setPokemonChoise] = useState({});
+    const urlApi = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    
+    const getApi = async () => {
+      try {
+        const response = await fetch(urlApi);
+        const data = await response.json();
+        setPokemonChoise(data);
+    } catch (error) {
+        console.error("Error fetching data from API:", error);
+    }
     }
 
-  return (
-    
-    <div>
-      pokemon seleccionado
-    </div>
-  )
+    useEffect(() => {
+      getApi();
+      
+    },[]);
+   
+    return (
+      <div className="d-flex flex-column align-items-center justify-content-center"
+      style={{ height: '100vh' }}>
+        {PokemonChoise && PokemonChoise.stats && (
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={`https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/home/${PokemonChoise.id}.png`} />
+            <Card.Body>
+              <Card.Title>{PokemonChoise.name}</Card.Title>
+              <Card.Text>
+                <ul>
+                  {PokemonChoise.stats.map((stat, index) => (
+                    <li key={index}>{stat.stat.name}: {stat.base_stat} </li>
+                  ))}
+                </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+      </div>
+    )
 }
 
 export default SelectPokemons
